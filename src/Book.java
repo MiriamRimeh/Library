5dbb582fbe99dc550c3375a1b8577feabccf01ed
MiriamRime <miriam.rimeh@gmail.com>
2024-02-03 19:35:04 +0200

    Initialiazing repository

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    String author;
+    String title;
+    String genre;
+    String description;
+    String releaseYear;
+    //key words should be array?
+    double rating;
+    String ISBN;
+}
